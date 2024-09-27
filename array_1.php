6de89278986d5b8a1afb6dab4c6e0e29ce8d8047
anton <antonios.kaharap@gmail.com>
2024-09-27 17:48:11 +0700

    first commit

---
+++ array_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php
+        $Listdosen = ["Elok Nur Hamdana", "Ungul Pamenang", "Bagas Nugraha"];
+
+        foreach ($Listdosen as $dosen) {
+            echo $dosen . "<br>";
+        }
+            
+    ?>
+</body>
+</html>
---
+++ array_2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+        table {
+            width: 70%;
+            margin: 20px auto;
+            border: 1px solid #333;
+            border-collapse: collapse;
+            margin-top: 200px;
+        }
+
+        th, td {
+            border: 1px solid #333;
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #333;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis_kelamin' => 'Perempuan'];
+            
+        echo "<table>";    
+        echo "<tr>
+                <td>Nama</td>
+                <td>{$Dosen['nama']}</td>
+            </tr>";
+        echo "<tr>
+            <td>Domisili</td>
+            <td>{$Dosen['domisili']}</td>
+        </tr>";
+        echo "<tr>
+            <td>Jenis Kelamin</td>
+            <td>{$Dosen['jenis_kelamin']}</td>
+        </tr>";
+        echo "</table>";
+    ?>
+</body>
+</html>
